bed48a8bb4fb8f9d134d77ea2b5911466159e21e
bm3182 <bm3182>
2021-05-21 09:55:24 +0900

    ALLOWD_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&3pk#e!5&^8-mun4g=m!e*eb(7lueyx^8pf#kqu+$ljq)$)&48'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [152.70.245.135]
 
 
 # Application definition
