b03491e8b49710eb1a22c27df3cd00d8427665e4
bm3182 <bm3182>
2021-05-24 14:05:57 +0900

    DB mysql 연결

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,13 @@ ALLOWED_HOSTS = ['152.70.245.135', 'bm3182.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pybo',
+        'USER': 'master',
+        'PASSWORD': 'P@ssw0rd',
+        'HOST': 'mysql-MDS1.subnet05120955.vcn05120955.oraclevcn.com',
+        'PORT': '3306',
+    }
+}
